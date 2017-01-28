2fcfbcb222cced855e48a02dc50f47485261b629
Jim O. Smith Jr. <mmtester@hotmail.com>
2017-01-27 18:35:29 -0600

    Testing uploader.

---
+++ GolfDB2/Models/UploadFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace GolfDB2.Models
+{
+    [Table("UploadFiles")]
+    public class UploadFiles
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
